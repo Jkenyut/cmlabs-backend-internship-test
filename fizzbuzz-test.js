a86a95b087085e5d12c04b01cce3550d4170fd60
Jkenyut <satrianursaputro06@gmail.com>
2022-09-21 22:02:46 +0700

    test 1 pretest FizzBuzz

---
+++ fizzbuzz-test.js
@@ -0,0 +1,18 @@
+// satria nur saputro
+// Intership Backend
+// looping sampe 100
+for (let i = 1; i < 101; i++) {
+  // jika i kelipatan  3 dan 5
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+    // jika i adalah kelipatan 3
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+    // jika i adalah kelipatan 5
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+    //jika i bukan pernyataan kelipatan
+  } else {
+    console.log(i);
+  }
+}
